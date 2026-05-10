3b618057e99c085547264f50e10c671a2d773e97
Yohannes <y.berhane56@gmail.com>
2026-05-10 19:33:34 +0000

    Number of nices strings non regex soln

--- src/main/scala/adventofcode/2015/5InternElves.scala
+++ src/main/scala/adventofcode/2015/5InternElves.scala
@@ -5,25 +5,57 @@
   *
   * A nice string is one with all of the following properties:
   *
-  * It contains at least three vowels (aeiou only), like aei, xazegov, or
-  * aeiouaeiouaeiou. It contains at least one letter that appears twice in a
-  * row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd). It does not
-  * contain the strings ab, cd, pq, or xy, even if they are part of one of the
-  * other requirements.
+  *   - It contains at least three vowels (aeiou only), like aei, xazegov, or
+  *     aeiouaeiouaeiou.
+  *   - It contains at least one letter that appears twice in a row, like xx,
+  *     abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+  *   - It does not contain the strings ab, cd, pq, or xy, even if they are part
+  *     of one of the other requirements.
   *
   * For example:
   *
-  * ugknbfddgicrmopn is nice because it has at least three vowels
-  * (u...i...o...), a double letter (...dd...), and none of the disallowed
-  * substrings. aaa is nice because it has at least three vowels and a double
-  * letter, even though the letters used by different rules overlap.
-  * jchzalrnumimnmhp is naughty because it has no double letter.
-  * haegwjzuvuyypxyu is naughty because it contains the string xy.
-  * dvszwmarrgswjxmb is naughty because it contains only one vowel.
+  *   - ugknbfddgicrmopn is nice because it has at least three vowels
+  *     (u...i...o...), a double letter (...dd...), and none of the disallowed
+  *     substrings.
+  *   - aaa is nice because it has at least three vowels and a double letter,
+  *     even though the letters used by different rules overlap.
+  *   - jchzalrnumimnmhp is naughty because it has no double letter.
+  *   - haegwjzuvuyypxyu is naughty because it contains the string xy.
+  *   - dvszwmarrgswjxmb is naughty because it contains only one vowel.
   *
   * How many strings are nice?
   */
 object Day05NumberOfNiceStrings:
+
+  def isNiceString(s: String): Boolean =
+    val isNice =
+      !(
+        // Doesn't contain any of these substrings
+        s.contains("ab") ||
+          s.contains("cd") ||
+          s.contains("pq") ||
+          s.contains("xy")
+      ) &&
+        (
+          // Contains atleast three vowles
+          s.sliding(1).count(x =>
+            x == "a" ||
+              x == "e" ||
+              x == "i" ||
+              x == "o" ||
+              x == "u"
+          ) >= 3
+        ) &&
+        (
+          s.sliding(2).count(x => x.size == 2 && x(0) == x(1)) > 0
+        )
+    println(s"$s is nice? $isNice")
+    isNice
+
   @main def numberOfNiceStrings(): Int =
     val lines: Seq[String] = os.read.lines(os.pwd / "AOC" / "15DAY05.txt")
-    0
+    val noOfNices = lines.foldLeft(0)({
+      case (nices, next) => if isNiceString(next) then nices + 1 else nices
+    })
+    println(s"Number of nices: $noOfNices")
+    noOfNices
